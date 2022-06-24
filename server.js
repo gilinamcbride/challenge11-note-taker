a2ecbb3b1594303f4972114c787d06d55f5b6988
Gilina McBride <gilinamcbride@gmail.com>
2022-06-23 23:07:48 -0400

    heroku error fix

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ app.delete("/api/notes/:id", (req, res) => {
 });
 
 // SET UP HTML ROUTES
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
